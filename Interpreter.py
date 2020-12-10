60008beb536245352332142e5a8b8b34f05c3e03
orionkepler <55416097+orionkepler@users.noreply.github.com>
2020-12-10 19:03:15 +0800

    Update Interpreter.py

--- Interpreter.py
+++ Interpreter.py
@@ -9,15 +9,16 @@ class Interpreter:
         
         # similar to symbol table, here it's only used to store variables' value
         self.symtab = {}
-        
+ 
         
     def statlist(self):
         while self.lexer.has_next():
             self.stat()
     
     def stat(self):
-        token_type, token_val = self.cur_token
-        
+        token_type = self.cur_token[0]
+        token_val = self.cur_token[1]
+
         # Asignment
         if token_type == TOKEN_GETER.ID:
             self.consume()
@@ -137,13 +138,108 @@ class Interpreter:
         elif token_type == TOKEN_GETER.COMMENT:
             self.consume()
             v = str(token_val)
-            print ('#' + v)
+            #print ('#' + v)
+
+        #if/else blocks
+        elif token_type == TOKEN_GETER.IF:
+            token_counter = self.cur_token[2]
+            self.consume()
+            while self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
+                self.consume()
+            sign = self.stat()
+            if sign == True:
+                while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                    self.consume()
+                self.consume()
+                while self.cur_token[2] - token_counter == 1:
+                    self.stat()
+                    if not self.lexer.has_next():
+                        break
+                if self.cur_token[0] == TOKEN_GETER.ELSE:
+                    token_counter1 = self.cur_token[2]
+                    self.consume()
+                    while self.cur_token[0] == TOKEN_GETER.SIMCOL:
+                        self.consume()
+                    while self.cur_token[2] - token_counter1 != 0:
+                        self.consume()
+                        while (self.cur_token[0] != TOKEN_GETER.PRINT and 
+                            self.cur_token[0] != TOKEN_GETER.IF and 
+                            self.cur_token[0] != TOKEN_GETER.ELSE and 
+                            self.cur_token[0] != TOKEN_GETER.ID):
+                            self.consume()
             
+            elif sign == False:
+                while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                    self.consume()
+                self.consume()
+                while self.cur_token[2] - token_counter != 0:
+                    self.consume()
+                    while (self.cur_token[0] != TOKEN_GETER.PRINT and 
+                            self.cur_token[0] != TOKEN_GETER.IF and 
+                            self.cur_token[0] != TOKEN_GETER.ELSE and 
+                            self.cur_token[0] != TOKEN_GETER.ID):
+                        self.consume()
+                if self.cur_token[0] == TOKEN_GETER.ELSE:
+                    token_counter1 = self.cur_token[2]
+                    self.consume()
+                    self.consume()
+                    while self.cur_token[2] - token_counter1 == 1:
+                        self.stat()
+                        if not self.lexer.has_next():
+                            break
+                    
+        elif token_type == TOKEN_GETER.ELIF:
+            token_counter = self.cur_token[2]
+            self.consume()
+            while self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
+                self.consume()
+            sign = self.stat()
+
+            if sign == True:
+                while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                    self.consume()
+                self.consume()
+                while self.cur_token[2] - token_counter == 1:
+                    self.stat()
+                    if not self.lexer.has_next():
+                        break
+                if self.cur_token[0] == TOKEN_GETER.ELSE:
+                    token_counter1 = self.cur_token[2]
+                    self.consume()
+                    while self.cur_token[0] == TOKEN_GETER.SIMCOL:
+                        self.consume()
+                    while self.cur_token[2] - token_counter1 != 0:
+                        self.consume()
+                        while (self.cur_token[0] != TOKEN_GETER.PRINT and 
+                            self.cur_token[0] != TOKEN_GETER.IF and 
+                            self.cur_token[0] != TOKEN_GETER.ELSE and 
+                            self.cur_token[0] != TOKEN_GETER.ID):
+                            self.consume()
             
+            elif sign == False:
+                while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                    self.consume()
+                self.consume()
+                while self.cur_token[2] - token_counter != 0:
+                    self.consume()
+                    while (self.cur_token[0] != TOKEN_GETER.PRINT and 
+                            self.cur_token[0] != TOKEN_GETER.IF and 
+                            self.cur_token[0] != TOKEN_GETER.ELSE and 
+                            self.cur_token[0] != TOKEN_GETER.ID):
+                        self.consume()
+                if self.cur_token[0] == TOKEN_GETER.ELSE:
+                    token_counter1 = self.cur_token[2]
+                    self.consume()
+                    self.consume()
+                    while self.cur_token[2] - token_counter1 == 1:
+                        self.stat()
+                        if not self.lexer.has_next():
+                            break
+    
         # print statement
         elif token_type == TOKEN_GETER.PRINT:
             self.consume()
-            if self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
+            while self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
                 self.consume()
             v = str(self.expr())
 
@@ -151,8 +247,9 @@ class Interpreter:
                 self.match(TOKEN_GETER.COMMA)
                 if self.cur_token[0] == TOKEN_GETER.STR_:
                     self.consume()
-                    self.consume()
-                v += ' ' + str(self.expr())
+                    while self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
+                        self.consume()
+                v += '' + str(self.expr())
                 if self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
                     self.consume()
 
@@ -160,11 +257,14 @@ class Interpreter:
                 self.match(TOKEN_GETER.ADD)
                 if self.cur_token[0] == TOKEN_GETER.STR_:
                     self.consume()
-                    self.consume()
-                v += ' ' + str(self.expr())
+                    while self.cur_token[0] == TOKEN_GETER.LPARENTHESES:
+                        self.consume()
+                v += '' + str(self.expr())
                 if self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
                     self.consume()
             print (v)
+            while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                self.consume()
 
 
         # add statement
@@ -172,25 +272,39 @@ class Interpreter:
             num = self.symtab.popitem()
             first_num = int(num[1])
             self.consume()
-            second_num = int(self.expr())
+            if self.cur_token[0] == TOKEN_GETER.SUBTRACT:
+                self.consume()
+                second_num = -int(self.expr())
+            else:
+                second_num = int(self.expr())
             result = first_num + second_num
             self.symtab[num[0]] = result
 
         # subtract statement
         elif token_type == TOKEN_GETER.SUBTRACT:
             num = self.symtab.popitem()
-            first_num = int(num[1])
-            self.consume()
-            second_num = int(self.expr())
-            result = first_num - second_num
-            self.symtab[num[0]] = result
+            if num[1] == None:
+                self.consume()
+                first_num = -int(self.cur_token[1])
+                self.symtab[num[0]] = first_num
+                self.consume()
+            else:
+                first_num = int(num[1])
+                self.consume()
+                second_num = int(self.expr())
+                result = first_num - second_num
+                self.symtab[num[0]] = result
 
         # times statement
         elif token_type == TOKEN_GETER.TIMES:
             num = self.symtab.popitem()
             first_num = int(num[1])
             self.consume()
-            second_num = int(self.expr())
+            if self.cur_token[0] == TOKEN_GETER.SUBTRACT:
+                self.consume()
+                second_num = -int(self.expr())
+            else:
+                second_num = int(self.expr())
             result = first_num * second_num
             self.symtab[num[0]] = result
 
@@ -199,7 +313,11 @@ class Interpreter:
             num = self.symtab.popitem()
             first_num = int(num[1])
             self.consume()
-            second_num = int(self.expr())
+            if self.cur_token[0] == TOKEN_GETER.SUBTRACT:
+                self.consume()
+                second_num = -int(self.expr())
+            else:
+                second_num = int(self.expr())
             result = first_num / second_num
             self.symtab[num[0]] = result
 
@@ -208,7 +326,11 @@ class Interpreter:
             num = self.symtab.popitem()
             first_num = int(num[1])
             self.consume()
-            second_num = int(self.expr())
+            if self.cur_token[0] == TOKEN_GETER.SUBTRACT:
+                self.consume()
+                second_num = -int(self.expr())
+            else:
+                second_num = int(self.expr())
             result = pow(first_num, second_num)
             self.symtab[num[0]] = result
 
@@ -217,7 +339,11 @@ class Interpreter:
             num = self.symtab.popitem()
             first_num = int(num[1])
             self.consume()
-            second_num = int(self.expr())
+            if self.cur_token[0] == TOKEN_GETER.SUBTRACT:
+                self.consume()
+                second_num = -int(self.expr())
+            else:
+                second_num = int(self.expr())
             result = first_num % second_num
             self.symtab[num[0]] = result
 
@@ -226,13 +352,15 @@ class Interpreter:
 
         elif self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
             self.consume()
-
+            while self.cur_token[0] == TOKEN_GETER.RPARENTHESES:
+                self.consume()
         else:
             raise Exception('not support token %s', token_type)
     
     
     def expr(self):
-        token_type, token_val = self.cur_token
+        token_type = self.cur_token[0]
+        token_val = self.cur_token[1]
         if token_type == TOKEN_GETER.STR:
             self.consume()
             return token_val
@@ -255,14 +383,40 @@ class Interpreter:
             return False
 #        raise Exception('expecting %s; found %s' % (token_type, self.cur_token[0]))
 
+ 
 
 if __name__ == '__main__':
     prog = '''
-        p = 7 ^ 2
-        t = 4 * 3 * 2
-        p %= t
-        print (p)
-        # This is a comment
+        # Pokemon master name
+        name = "Ash Ketchum"
+
+        # Pokemon Health Points
+        charmender_HP = 110
+        squirtle_HP = 125
+        bulbasaur_HP = 150
+
+        # Pokemon Attack Points
+        charmender_attack = 40
+        squirtle_attack = 35
+        bulbasaur_attack = 25
+
+        eq1 = 2 * -5 + 20
+        print("EQ1: "+str(eq1))
+        if(eq1 != 0):
+            print("eq1 output not equal to 0")
+        eq2 = -2 * 3 / 12
+        print("EQ2: "+str(eq2))
+
+        if charmender_HP >= 1:
+            print(name+"'s Charmender won!")
+        elif squirtle_HP >=1:
+            print(name+"'s Squirtle won!")
+        else:
+            print("Something went wrong!!!")
+
+        print ("name", name)
+        print (name, "name")
+        print ("Charmender did "+str (charmender_attack)+" damage")
 
     '''
     lex = TOKEN_GETER.TOKEN_GETER(prog)
